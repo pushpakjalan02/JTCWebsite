e5ba558ba1309f24bef5a2d70b9add843000aba4
Pushpak Jalan <pushpakjalan07@outlook.com>
2021-01-11 19:03:23 +0530

    Finished welcome part of home component

--- home.php
+++ home.php
@@ -1,8 +1,25 @@
 <?php include 'NavigationComponent/navigation-component.php' ?>
 <?php
     echo 
-        "<div class='home'>
-            <div class='welcome'>
+        "<div class='home container-fluid'>
+            <div class='welcome row'>
+                <div class='welcome-p1 col-12 col-sm-6 align-self-center'>
+                    <span class='row message-primary'>Your Needs, Our Priority</span>
+                    <span class='row message-secondary'>Experience the best-in class service</span>
+                    <div class='row justify-content-center'>
+                        <a class='col-5 m-2 btn btn-warning' href='tel:+919435052955' role='button'>
+                            <i class='fa fa-phone' aria-hidden='true'></i>
+                            Get in Touch
+                        </a>
+                        <a class='col-5 m-2 btn btn-outline-warning' href='#' role='button'>
+                            <i class='fa fa-paper-plane' aria-hidden='true'></i>
+                            Explore
+                        </a>
+                    </div>
+                </div>
+                <div class='welcome-p2 col-12 col-sm-6 align-self-center'>
+                    <img src='assets\welcome.png' class='image img-fluid mx-auto d-block' alt='Welcome image'>
+                </div>
             </div>
             a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>
             a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>a<br/>
