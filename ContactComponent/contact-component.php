1de55b807fddb7ef919c21f231b9d9207905b22c
Pushpak Jalan <pushpakjalan07@outlook.com>
2021-02-07 23:15:57 +0530

    Added Contact Component

---
+++ ContactComponent/contact-component.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>    
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="../NavigationComponent/navigation-component.css?<?php echo time(); ?>" type="text/css">
+        <link rel="stylesheet" href="../FooterComponent/footer-component.css?<?php echo time(); ?>" type="text/css">
+        <link rel="stylesheet" href="contact-component.css?<?php echo time(); ?>" type="text/css">
+        <title>Jorhat Trade Centre</title>
+        <link rel="icon" href="assets/JTC_Logo.png" type="image/x-icon">
+    </head>
+    <body>
+        <?php include '../NavigationComponent/navigation-component.php' ?>
+        
+        <div class="contact container-fluid">
+            <div class="contact-content row no-gutters align-items-center">
+                <div class="contact-details col-md-5">
+                    <h1 class="text-center">Reach Us</h1>
+                    <div class="text-center">
+                        Jorhat Trade Centre<br/>
+                        Opposite NRL Petrol Pump and ISBT<br/>
+                        Near Kataki Pukhuri<br/>
+                        A.T. Road<br/>
+                        Jorhat (West) - Assam<br/>
+                        Pin Code: 785001<br/>
+                        <a href="tel:+919435052955"><i class="fa fa-phone"></i> +91-9435052955<br/></a>
+                        <a href="mailto:jorhattradecentre@gmail.com"><i class="fa fa-envelope"></i> jorhattradecentre@gmail.com</a>
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <h1 class="text-center">Or</h1>
+                </div>
+                <div class="social-media-links col-md-5">
+                    <h1 class="text-center">Find Us On</h1>
+                    <div class="text-center">
+                        <a href="https://g.page/jorhat-trade-centre?share"><i class="fa fa-map-marker" aria-hidden="true"></i></a>
+                        <a href="https://wa.me/919435052955"><i class="fa fa-whatsapp" aria-hidden="true"></i></a>
+                        <a href="https://t.me/jorhattradecentre"><i class="fa fa-telegram" aria-hidden="true"></i></a>
+                        <a href="https://www.facebook.com/jtc.tally/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                        <a href="https://twitter.com/JTC_Jorhat"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                        <a href="https://www.linkedin.com/company/jorhattradecentre"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                        <a href=""><i class="fa fa-instagram" aria-hidden="true"></i></a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php include '../FooterComponent/footer-component.php' ?>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    </body>
+</html>
